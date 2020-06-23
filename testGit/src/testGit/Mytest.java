c4b4ff32611e3749fee8a9cc6de7b0f782915916
MincheolChae <cmc752@gmail.com>
2020-06-23 17:52:33 +0900

    수정해따

--- testGit/src/testGit/Mytest.java
+++ testGit/src/testGit/Mytest.java
@@ -2,7 +2,7 @@ package testGit;
 
 public class Mytest {
 	public static void main(String[] args) {
-		System.out.println("깃 연동!");
-		System.out.println("연동수정");
+		System.out.println("깃... 연동...");
+		System.out.println("연동...수정...");
 	}
 }
